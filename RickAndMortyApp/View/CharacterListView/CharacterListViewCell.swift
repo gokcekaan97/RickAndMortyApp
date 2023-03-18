c598928bd5f60f1149accaf918bea77dcdd4a3c9
Kaan GÖKÇEK <kaan.gokcek@halkbank.com.tr>
2023-03-18 12:58:45 +0300

    custom viewcell created for collectionview

---
+++ RickAndMortyApp/View/CharacterListView/CharacterListViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CharacterListViewCell.swift
+//  RickAndMortyApp
+//
+//  Created by kaan gokcek on 18.03.2023.
+//
+
+import UIKit
+
+class CharacterListViewCell: UICollectionViewCell {
+    
+}
